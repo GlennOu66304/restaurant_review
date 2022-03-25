dbcfbbfe415c3504ee1dc26b3c183e337d334c46
Glenn Ou <2738328919@qq.com>
2022-03-26 01:18:05 +0800

    axios

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,6 +1,9 @@
+
+//1.import the aixo package
 import axios from "axios";
 
-export default axios.create({
+// 2.export the axios instance, axios.create() is a funtion
+export default axios.create({// include a object
     baseURL: "http://localhost:5000/api/v1/",
     headers: {
         "Content-type": "application/json"
